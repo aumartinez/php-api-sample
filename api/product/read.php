260800502a11cc28a79006b4aa4e69d14031fb86
Augusto Martinez <acm_asd@hotmail.com>
2020-08-04 20:55:53 -0600

    Update read.php

--- api/product/read.php
+++ api/product/read.php
@@ -3,4 +3,14 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json; charset=UTF-8");
 
+require_once ("../config/database.php");
+require_once ("../objects/product.php");
+
+$database = new Database();
+$db = $database->open_link();
+
+$product = new Product(string $db);
+
+$stmt = $product->read();
+
 ?>
