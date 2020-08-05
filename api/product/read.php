e4e9ea41242a2d08fc1d7cf2af2b60d5d742cd29
Augusto Martinez <acm_asd@hotmail.com>
2020-08-04 21:05:57 -0600

    Update read.php

--- api/product/read.php
+++ api/product/read.php
@@ -1,7 +1,7 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-type: application/json; charset=UTF-8");
+header ("Access-Control-Allow-Origin: *");
+header ("Content-type: application/json; charset=UTF-8");
 
 require_once ("../config/database.php");
 require_once ("../objects/product.php");
@@ -12,5 +12,42 @@ $db = $database->open_link();
 $product = new Product(string $db);
 
 $stmt = $product->read();
+$rows = $stmt->rowCount();
+
+if ($rows > 0) {
+  
+  $prod_arr = array();
+  $prod_arr["records"] = array();
+  
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    extract($row);
+    
+    $prod_item = array(
+    "id" => $id,
+    "name" => $name,
+    "description" => html_entity_decode($description),
+    "price" => $price,
+    "category_id" => $cat_id,
+    "category_name" => $cat_name,
+    );
+    
+    $prod_arr["records"] = $prod_item;
+  }
+  
+  http_response_code(200);
+  
+  echo json_encode($prod_arr);
+  
+} 
+else {
+  
+  http_response_code(400);
+  
+  $mess = array(
+  "message" => "No products found",
+  );
+  
+  echo json_encode($mess);
+}
 
 ?>
